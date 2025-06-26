459659d31710ec9d34f97dd425057d9fcb7e6f1a
prathi620 <prathikutty620@gmail.com>
2025-06-26 06:46:17 -0700

    commit 0127

---
+++ function.js
@@ -0,0 +1,11 @@
+//functions
+
+function calculator(opertion, num1, num2){
+//   if(operation == 'add') {
+        console.log('add' =>, num1 + num2);
+//   } else if (opertion, == 'sub') {
+        console.log('sub' =>, num1 - num2);
+   
+// }
+// calculator('add', 10, 13)
+console.log()
